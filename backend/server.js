fda01292bab13f2a8042da47e9de4e8afe909b9d
Adetunji Marvellous <adetunjimarvellous09@gmail.com>
2026-08-16 18:17:45 +0100

    Add console log for database connection status

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ process.on('uncaughtException', (err) => {
 });
 
 const DB = process.env.DB;
+console.log('Connecting to DB...', DB);
 
 mongoose.connect(DB).then(() => {
   console.warn('DB connection successful ✅');
